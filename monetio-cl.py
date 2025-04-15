3a7241ffc060bb174b462543eb964df86691075e
AntonioTorga <antonio.torga@ug.uchile.cl>
2025-04-15 13:49:31 -0400

    further development of classes

---
+++ monetio-cl.py
@@ -0,0 +1,31 @@
+class MonetioCL:
+    def __init__(self):
+        pass
+    
+    def get_sinca(self, start_date, end_date):
+        """
+        Get the SINCA data from the start_date to the end_date.
+        """
+        # Implement the logic to get the SINCA data
+        pass
+
+    def get_dmc(self, start_date, end_date):
+        """
+        Get the DMC data.
+        """
+        # Implement the logic to get the DMC data
+        pass
+        
+    def process_sinca(self, path):
+        """
+        Process the SINCA data.
+        """
+        # Implement the logic to process the SINCA data
+        pass
+    
+    def process_dmc(self, path):
+        """
+        Process the DMC data.
+        """
+        # Implement the logic to process the DMC data
+        pass
--- translate/translator.py
+++ translate/translator.py
@@ -1,8 +1,14 @@
-
+from abc import abstractmethod
 from pathlib import Path
 from dask import dataframe as dd
 
 class NetcdfTranslator:
+    """Base class for translating data files to NetCdf format.
+    Accumulates data in self.data and then proceeds to save it in NetCDF format.
+    The data is loaded from the input_path and saved to the output_path.
+    The filename_format is used to match the files in the input_path.
+    The input_path and output_path are expected to be directories.
+    """
     def __init__(self, input_path, output_path, filename_format):
         self.filename_format = filename_format
 
@@ -10,6 +16,11 @@ class NetcdfTranslator:
         self.output_path = Path(output_path)
         self.data = None
 
+    @abstractmethod
+    def process_df(self, df):
+        pass
+    
+    @abstractmethod
     def load_file(self, file):
         pass
 
@@ -22,7 +33,7 @@ class NetcdfTranslator:
         return self.data
     
     def to_netcdf(self):
-        pass
+        self.data.to_netcdf(self.output_path / f"{self.input_path.stem}.nc", format="NETCDF4", unlimited_dims="time")
     
     def run(self):
         self.load_data()
